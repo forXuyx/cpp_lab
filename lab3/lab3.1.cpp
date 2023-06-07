12429a373850e9206953ac782e61c1d64ecc9c74
forXuyx <xuyuxiang22@mails.ucas.ac.cn>
2023-06-07 22:58:02 +0800

    lab3.3

--- lab3/lab3.1.cpp
+++ lab3/lab3.1.cpp
@@ -1,3 +1,5 @@
 //
 // Created by xyx on 23-6-7.
 //
+
+// Easy linux commends
---
+++ lab3/lab3.3.cpp
@@ -0,0 +1,17 @@
+//
+// Created by xyx on 23-6-7.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << sizeof(size_t) << endl;
+//    for(size_t n = 2; n >= 0; n--)
+//        cout << "n = " << n << "  ";
+
+    return 0;
+
+}
+
+// use size_t will be a loop
