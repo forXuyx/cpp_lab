68adb46a6ef38e50c23198a667820c55bf53cd13
forXuyx <xuyuxiang22@mails.ucas.ac.cn>
2023-06-07 22:40:58 +0800

    lab2.5

---
+++ lab2/lab2.5.cpp
@@ -0,0 +1,19 @@
+//
+// Created by xyx on 23-6-7.
+//
+#include <iostream>
+
+int main()
+{
+    auto a = 10;
+    a = 20.5;
+    a += 10.5;
+
+    std::cout << a << std::endl;
+
+    return 0;
+
+}
+
+// the answer will be 30
+// ok, I got it.
